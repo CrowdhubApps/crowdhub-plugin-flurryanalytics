2b6da755bae35c53b4388f83ad5628e25c9d0fb4
Austin Isakson <austini@crowdhubapps.com>
2022-11-04 12:02:08 -0700

    building out custom events, advanced features, and the initialization in ios

--- ios/Plugin/FlurryAnalyticsPlugin.swift
+++ ios/Plugin/FlurryAnalyticsPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import Flurry_iOS_SDK
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,10 +10,69 @@ import Capacitor
 public class FlurryAnalyticsPlugin: CAPPlugin {
     private let implementation = FlurryAnalytics()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    @objc func initialize(_ call: CAPPluginCall) {
+        // TODO:
+        let logLevel = call.getString("logLevel", "DEBUG")
+        let crashReportingEnabled = call.getBool("crashReportingEnabled", true)
+        let appVersion = call.getString("appVersion", "1.0")
+        let iapReportingEnabled = call.getBool("iapReportingEnabled", true)
+        let apiKey = call.getString("apiKey")
+        
+        let sb = FlurrySessionBuilder()
+                  .build(logLevel: FlurryLogLevel.all)
+                  .build(crashReportingEnabled: crashReportingEnabled)
+                  .build(appVersion: appVersion)
+                  .build(iapReportingEnabled: iapReportingEnabled)
+              
+            Flurry.startSession(apiKey: apiKey, sessionBuilder: sb)
+    }
+    
+    
+    
+    //StandardEvents: https://developer.yahoo.com/flurry/docs/analytics/standard_events/iOS/
+    
+    
+    
+    
+    // Custom Events: https://developer.yahoo.com/flurry/docs/analytics/gettingstarted/events/ios/
+    @objc func logCustomEvent(_ call: CAPPluginCall) {
+        let eventName = call.getString("eventName","")
+        let eventParams = call.getObject("eventParams", [:])
+        let eventTimed = call.getBool("eventTimed",false)
         call.resolve([
-            "value": implementation.echo(value)
+            "eventName": Flurry.log(eventName: eventName, parameters: eventParams, timed: eventTimed)
+        ])
+    }
+    
+    
+    // Advanced Features: https://developer.yahoo.com/flurry/docs/analytics/gettingstarted/technicalquickstart/ios/
+    @objc func setUserId(_ call: CAPPluginCall) {
+        let userId = call.getString("userId","")
+        call.resolve([
+            "userId": Flurry.set(userId: userId)
+        ])
+    }
+
+    @objc func setAge(_ call: CAPPluginCall) {
+        let userAge = call.getInt("userAge",0)
+        call.resolve([
+            "userAge": Flurry.set(age: userAge)
+        ])
+    }
+
+    @objc func setGender(_ call: CAPPluginCall) {
+        let userGender = call.getString("userGender", "")
+        call.resolve([
+            "userGender": Flurry.set(gender: userGender)
+        ])
+    }
+
+    @objc func logError(_ call: CAPPluginCall) {
+        let errorId = call.getString("errorId", "")
+        let errorMessage = call.getString("errorMessage", "")
+        let error = call.getString("error", "")
+        call.resolve([
+            "error": Flurry.log(errorId: errorId, message: errorMessage, error: error)
         ])
     }
 }
